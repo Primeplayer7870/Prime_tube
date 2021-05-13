b2e132488db36c59df936a0972c22ea1e488c3d3
Primeplayer7870 <84000746+Primeplayer7870@users.noreply.github.com>
2021-05-13 12:52:58 +0530

    Create about.sh

---
+++ files/other_files/about.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+R='\033[31m'
+GRE='\033[32m'
+W='\033[37m'
+B='\033[34m'
+GRA='\033[1;30m'
+Y='\033[1;33m'
+C='\033[1;36m'
+
+echo -e ${W}"Author:    ${W}PRIMEPLAYER"
+echo -e ${R}"Name:      ${W}Prime_tube"
+echo
+echo -e ${R}"Contact:   ${W}Primeplayer7870@gmail.com"
+echo
+echo -e ${W}"If you find errors/bugs, please contact me,I will try to fix it :)${W}"
+echo

9bab0fd4ad685f570917463e6779e07b6b7a6099
Primeplayer7870 <84000746+Primeplayer7870@users.noreply.github.com>
2021-05-13 12:54:01 +0530

    Create change.sh

---
+++ files/other_files/change.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+R='\033[31m'
+GRE='\033[32m'
+W='\033[37m'
+B='\033[34m'
+GRA='\033[1;30m'
+Y='\033[1;33m'
+C='\033[1;36m'
+
+echo -e ${GRE}"Version 1.0${White}"
+echo "- First version, no changes"
+echo -e ${W} "Notice-New features coming in Version 2.0"
+echo " "
+echo -e ${W} "You can contribute something to this project"
+echo -e ${W} "and you can get credit in and in github also :]"

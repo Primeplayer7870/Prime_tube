2b92912f77b13a3f11f73665111089f184193712
Primeplayer7870 <84000746+Primeplayer7870@users.noreply.github.com>
2021-05-13 12:44:53 +0530

    Create banner.sh

---
+++ files/banner.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+R='\033[31m'
+W='\033[37m'
+B='\033[34m'
+GRA='\033[1;30m'
+Y='\033[1;33m'
+C='\033[1;36m'
+GRE='\033[32m'
+echo -e ${B} "&&&&&           &                           &           &            "
+echo -e ${B} "&    &  &&&&  &&&    &&&&&   &&&          &&&&&  &   &  &       &&&  "
+echo -e ${B} "&&&&&   &   &   &    & & &  &   &           &    &   &  &&&&&  &   & "
+echo -e ${B} "&       &       &    & & &  &''''           &    &   &  &   &  &'''' "
+echo -e ${B} "&       &     &&&&&  & & &  &&&&&           &&&  &&&&&  &&&&&  &&&&& "
+echo
+echo -e ${B} "                                  &&&&&&&&&"
+echo " "
+echo -e ${GRE} "                         # # # # # # # # # # # # # # "
+echo  " "
+echo -e ${GRE} "                         #  Prime_tube version 1.0 # "
+echo " "
+echo -e ${GRE} "                         # # # # # # # # # # # # # # "
+echo " "
+echo " "
+echo -e ${W} "           Welcome to this youtube video downloader- Prime_tube"
+echo " "
+echo -e ${Y} "                           MADE BY- Primeplayer"
+echo " "
+echo -e ${Y} "             Made with-Termux,ES File Explorer(ES note editor)"
